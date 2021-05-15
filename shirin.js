9da668dae034385de2e540f3972b18735f0a80c7
BANI GIT <bani.rezaie@gmail.com>
2021-05-15 14:11:25 +0100

    shirin

--- shirin.js
+++ shirin.js
@@ -5,5 +5,14 @@ if (x <= 100) {
 }
 if (!(x > 100)) {
 }
+// 3- empty this array:
+let arrayList = ["a", "b", "c", "d", "e", "f"];
 
-//3- just in time question.
+// 4- Calculate the length of the associative array
+var counterArray = {
+  A: 3,
+  B: 4,
+};
+counterArray["C"] = 1;
+
+// 5- just in time question.
