b1812c4d44f33c783e6c20c6f887f5152fed16f3
BANI GIT <bani.rezaie@gmail.com>
2021-05-15 13:25:33 +0100

    fatemeh

---
+++ fatemeh.js
@@ -0,0 +1,13 @@
+// 1- What's the difference between undefined and not defined in JavaScript
+
+// 2- empty this array:
+let arrayList = ["a", "b", "c", "d", "e", "f"];
+
+// 3- Calculate the length of the associative array
+var counterArray = {
+  A: 3,
+  B: 4,
+};
+counterArray["C"] = 1;
+
+// 4- just in time question
